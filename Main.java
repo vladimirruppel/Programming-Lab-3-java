b1e4d7b93ee08dbb8dc48c086c37c67562a75a98
vladimirruppel <vladimir.ruppel2004@mail.ru>
2023-10-22 20:17:51 +0700

    Создание класса AudioPlayer; пример работы в Main

--- Main.java
+++ Main.java
@@ -3,6 +3,13 @@ public class Main {
         AudioFile af1 = new AudioFile("main1.mp3", "Drake", "God's plan", 243, 2023);
 		AudioFile af2 = new AudioFile("main2.mp3", "Kanye West", "Jesus Lord", 243, 2021);
 
-		Playlist pl = new Playlist()
+		Playlist pl = new Playlist("Крутой рэп из Америки", af1, af2);
+		pl.PrintTrackList();
+
+		AudioPlayer player = new AudioPlayer(pl);
+		player.PrintInfo();
+		player.Play();
+		player.Next();
+		player.PrintInfo();
     }
 }
