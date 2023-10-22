c565fe1725e4927e6c21f7c93f43fcadd3cef88e
vladimirruppel <vladimir.ruppel2004@mail.ru>
2023-10-22 18:36:58 +0700

    Добавлен класс Playlist

--- AudioFIle.java
+++ AudioFIle.java
@@ -5,6 +5,7 @@ public class AudioFile {
 	private int duration;
 	private int releaseYear;
 
+	// конструктор
 	public AudioFile(String fileName, String artistName, String trackName, int duration, int releaseYear) {
 		this.fileName = fileName;
 		this.artistName = artistName;
@@ -13,7 +14,67 @@ public class AudioFile {
 		this.releaseYear = releaseYear;
 	}
 
+	// сеттеры
+	public void SetFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public void SetArtistName(String artistName) {
+		this.artistName = artistName;
+	}
+
+	public void SetTrackName(String trackName) {
+		this.trackName = trackName;
+	}
+
+	public void SetDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public void SetReleaseYear(int releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+	// геттеры
+	public String GetFileName() {
+		return fileName;
+	}
+	
+	public String GetArtistName() {
+		return artistName;
+	}
+
+	public String GetTrackName() {
+		return trackName;
+	}
+
+	public int GetDuration() {
+		return duration;
+	}
+
+	public int GetReleaseYear() {
+		return releaseYear;
+	}
+
+	// методы
 	public void PrintInfo() {
-		System.out.println(fileName + " " + artistName + " " + trackName + " " + duration + " " + releaseYear);
+		PrintInfoLong();
+	}
+
+	public void PrintInfo(boolean isLong) {
+		if (isLong) PrintInfoLong();
+		else PrintInfoShort();
+	}
+
+	public void PrintInfoLong() {
+		System.out.println("Название файла: " + fileName);
+		System.out.println("Имя артиста: " + artistName);
+		System.out.println("Название трека: " + trackName);
+		System.out.println("Длительность: " + duration);
+		System.out.println("Год выхода: " + releaseYear);
+	}
+
+	public void PrintInfoShort() {
+		System.out.println(artistName + " - " + trackName + ", " + duration + "s, " + releaseYear + " - " + fileName);
 	}
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        AudioFile af = new AudioFile("main1.mp3", "Drake", "God's plan", 243, 2023);
+        AudioFile af1 = new AudioFile("main1.mp3", "Drake", "God's plan", 243, 2023);
+		AudioFile af2 = new AudioFile("main2.mp3", "Kanye West", "Jesus Lord", 243, 2021);
 
-		af.PrintInfo();
+		Playlist pl = new Playlist()
     }
 }
